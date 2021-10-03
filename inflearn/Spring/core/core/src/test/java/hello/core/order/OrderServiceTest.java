0b455096d8e9f6569616f0459ee670ef0f1689a8
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-10-03 22:49:48 +0900

    core

---
+++ inflearn/Spring/core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+        Assertions.assertEquals(order.getDiscountPrice(),1000);
+    }
+}
