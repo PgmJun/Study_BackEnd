e6ad43c07f237f2c8626e5d93556b5ed790d79e1
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-10-04 23:28:02 +0900

    update

--- inflearn/Spring/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ inflearn/Spring/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,20 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
