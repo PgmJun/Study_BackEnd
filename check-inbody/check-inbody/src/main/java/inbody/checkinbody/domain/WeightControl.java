b2b172b6680fa86a1b8075e26501efba4fb19fba
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-10-13 00:08:23 +0900

    update-WeightControlDomain

--- check-inbody/check-inbody/src/main/java/inbody/checkinbody/domain/WeightControl.java
+++ check-inbody/check-inbody/src/main/java/inbody/checkinbody/domain/WeightControl.java
@@ -8,13 +8,13 @@ public class WeightControl {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)//IDENTITY: 쿼리에 값을 넣으면 DB가 자동으로 값을 채워주는 것
     private long num;
 
-    private String date;
-    private double currentWeight;
-    private double goalWeight;
-    private String startDate;
-    private String finishDate;
-    private long totalDate;
-    private double bmr; //기초대사량
+    private String id; //유저 ID
+    private String date; //기록 날짜
+    private double currentWeight; //현재 몸무게
+    private double goalWeight; //목표 몸무게
+    private String startDate; //체중조절 시작일
+    private String finishDate; //체중조절 종료일
+    private long totalDate; //종료일 - 시작일
     private double wlm; //감량대사량
 
     public String getDate() {
@@ -65,19 +65,19 @@ public class WeightControl {
         this.totalDate = totalDate;
     }
 
-    public double getBmr() {
-        return bmr;
-    }
-
-    public void setBmr(double bmr) {
-        this.bmr = bmr;
-    }
-
     public double getWlm() {
         return wlm;
     }
 
     public void setWlm(double wlm) {
         this.wlm = wlm;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
