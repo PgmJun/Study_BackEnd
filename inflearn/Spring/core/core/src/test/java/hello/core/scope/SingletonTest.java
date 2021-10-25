5e87dc6b6e963c37119589faf6a798bc2a70ea03
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-10-25 23:45:44 +0900

    Prototype scope added

---
+++ inflearn/Spring/core/core/src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+import static org.assertj.core.api.Assertions.assertThat;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(singletonBean.class);
+        singletonBean singletonBean1 = ac.getBean(singletonBean.class);
+        singletonBean singletonBean2 = ac.getBean(singletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+
+    }
+
+    @Scope("singleton")
+    static class singletonBean {
+        @PostConstruct
+        public void init(){
+            System.out.println("singletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("singletonBean.destroy");
+        }
+    }
+}
