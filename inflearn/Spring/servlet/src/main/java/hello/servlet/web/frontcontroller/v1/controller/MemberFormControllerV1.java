aebde33369352c97cc58edc3b99c099372a2593d
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-12-12 23:26:09 +0900

    Spring MVC패턴 학습
    servlet 프론트 컨테이너 추가.

---
+++ inflearn/Spring/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        //RequestDispatch:컨트롤러에서 뷰로 이동할때 쓰이는 것
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ inflearn/Spring/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        //Model에 members저장
+        request.setAttribute("members", members);
+
+        //View경로 지정
+        String viewPath = "/WEB-INF/views/members.jsp";
+        //dispatcher로 View호출
+        return new MyView(viewPath);
+    }
+}
