7d45d0f52b1b53d59aaa65788d96ac831fc5cd7d
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-06 00:20:48 +0900

    Thymeleaf 학습중 #5

--- inflearn/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ inflearn/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -48,12 +51,46 @@ public class BasicController {
 
     }
 
+    @GetMapping("basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+        return "basic/link";
+
+    }
+
+    @GetMapping("operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+    }
+
+
     @Data
     static class User {
         private String username;
         private int age;
 
-        public User(String username, int age){
+        public User(String username, int age) {
             this.username = username;
             this.age = age;
         }
