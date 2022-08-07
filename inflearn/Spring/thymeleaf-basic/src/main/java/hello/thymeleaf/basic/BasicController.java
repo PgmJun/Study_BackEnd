33353680aa4504b696ca8b4c069e8a52ad4f2321
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-08 00:15:10 +0900

    Thymeleaf 반복,조건부평가 #5

--- inflearn/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ inflearn/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -90,6 +90,27 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
 
     @Data
     static class User {
